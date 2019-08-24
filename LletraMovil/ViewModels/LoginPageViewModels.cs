071a9af620650667010573cd14ec5c53c43d9f69
Gad <garenas@sysware.com.mx>
2019-08-24 15:52:18 -0500

    Login
    Se agrega funcionalidad para acciones de login, aun no consulta db.

---
+++ LletraMovil/ViewModels/LoginPageViewModels.cs
@@ -0,0 +1,41 @@
+﻿using LletraMovil.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace LletraMovil.ViewModels
+{
+    public class LoginPageViewModels : ViewModelBase
+    {
+        public INavigation Navigation { get; internal set; }
+        public Command CmdRegresar { get; internal set; }
+        public Command CmdRegistrar { get; internal set; }
+        public Command CmdRecuperar { get; internal set; }
+
+
+        public LoginPageViewModels()
+        {
+            CmdRegresar = new Command(Regresar);
+            CmdRegistrar = new Command(Registrar);
+            CmdRecuperar = new Command(Recuperar);
+        }
+
+        void Regresar()
+        {
+            Preferences.Set("TypeUser", null);
+            Application.Current.MainPage = new NavigationPage(new SelectTypeUserPage());
+        }
+
+        void Registrar()
+        {
+            Application.Current.MainPage = new NavigationPage(new RegisterPage());
+        }
+
+        void Recuperar()
+        {
+            
+        }
+    }
+}
--- LletraMovil/Views/LoginPage.xaml.cs
+++ LletraMovil/Views/LoginPage.xaml.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿using LletraMovil.ViewModels;
+using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace LletraMovil.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        private LoginPageViewModels viewModel;
         public LoginPage()
         {
             InitializeComponent();
+            BindingContext = viewModel = new LoginPageViewModels();
+            viewModel.Navigation = this.Navigation;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
