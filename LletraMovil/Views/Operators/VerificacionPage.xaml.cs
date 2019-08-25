8474c824e980852158963bd14952494ccedaa903
Aleymarth <aleymarth@gmail.com>
2019-08-24 23:22:53 -0500

    Verificacion
    Se agrega la verificacion

---
+++ LletraMovil/Views/Operators/VerificacionPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LletraMovil.Views.Operators
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VerificacionPage : ContentPage
+    {
+        public VerificacionPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
